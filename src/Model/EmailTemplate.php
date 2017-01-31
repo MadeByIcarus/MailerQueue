9987b9d74711db81cb0ab7cb6030ecb8c15eee68
Pavel Gajdoš <info@pavelgajdos.cz>
2017-01-31 10:40:37 +0100

    Added email template

---
+++ src/Model/EmailTemplate.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavelgajdos
+ * Date: 31.01.17
+ * Time: 8:56
+ */
+
+namespace Icarus\QueueMailer\Model;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Kdyby\Doctrine\Entities\Attributes\Identifier;
+
+
+/**
+ * @ORM\Entity
+ */
+class EmailTemplate
+{
+
+    use Identifier;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $language;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $from;
+
+
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+}
